eaf957222463ba05b628e4f4c34a8a127536b6b6
Nick DeGroot <nbdegroot1@gmail.com>
2017-04-26 15:21:13 -0700

    Made more settings actually change things

--- app/src/main/java/com/nbdeg/unityplanner/utils/AlarmReceiver.java
+++ app/src/main/java/com/nbdeg/unityplanner/utils/AlarmReceiver.java
@@ -26,58 +26,63 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
-        // Notification for each due assignment
-        Database.dueAssignmentsDb.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                int numberOfAssignments = 0;
-                Notification.Builder builder = new Notification.Builder(context);
-                Notification.InboxStyle inboxStyle = new Notification.InboxStyle();
+        if (prefs.getBoolean("notifications_new_message", true)) {
+            // Notification for each due assignment
+            Database.dueAssignmentsDb.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    int numberOfAssignments = 0;
+                    Notification.Builder builder = new Notification.Builder(context);
+                    Notification.InboxStyle inboxStyle = new Notification.InboxStyle();
 
-                int daysInAdvance = Integer.parseInt(prefs.getString("notification_days_in_advance", "1"));
+                    int daysInAdvance = Integer.parseInt(prefs.getString("notification_days_in_advance", "1"));
 
-                Calendar cal = Calendar.getInstance();
-                cal.setTimeInMillis(System.currentTimeMillis());
-                cal.add(Calendar.DATE, daysInAdvance);
-                cal.set(Calendar.HOUR_OF_DAY, 23);
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTimeInMillis(System.currentTimeMillis());
+                    cal.add(Calendar.DATE, daysInAdvance);
+                    cal.set(Calendar.HOUR_OF_DAY, 23);
 
-                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
-                    Assignments assignment = userSnapshot.getValue(Assignments.class);
-                    if (assignment.getDueDate() < cal.getTimeInMillis()) {
-                        // Assignment due next day, notify user about that
-                        numberOfAssignments++;
-                        inboxStyle.addLine(assignment.getAssignmentName());
+                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                        Assignments assignment = userSnapshot.getValue(Assignments.class);
+                        if (daysInAdvance == 0) {
+                            numberOfAssignments++;
+                            inboxStyle.addLine(assignment.getAssignmentName());
+                        } else if (assignment.getDueDate() < cal.getTimeInMillis()) {
+                            // Assignment due next day, notify user about that
+                            numberOfAssignments++;
+                            inboxStyle.addLine(assignment.getAssignmentName());
+                        }
+                    }
+
+                    if (numberOfAssignments != 0) {
+                        builder.setSmallIcon(R.drawable.ic_logo);
+                        builder.setAutoCancel(true);
+
+                        //to be able to launch your activity from the notification
+                        Intent notifyIntent = new Intent(context, MainActivity.class);
+                        PendingIntent pendingIntent = PendingIntent.getActivity(context, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        builder.setContentIntent(pendingIntent);
+                        builder.setSound(Uri.parse(prefs.getString("notifications_new_message_ringtone", "content://settings/system/notification_sound")));
+                        if (prefs.getBoolean("notifications_new_message_vibrate", true)) {
+                            builder.setVibrate(new long[]{1000});
+                        } else {
+                            builder.setVibrate(new long[]{0});
+                        }
+
+                        inboxStyle.setBigContentTitle(numberOfAssignments + " assignments due tomorrow");
+                        inboxStyle.setSummaryText(numberOfAssignments + " assignments due tomorrow");
+                        builder.setStyle(inboxStyle);
+
+                        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+                        managerCompat.notify(NOTIFICATION_ID, builder.build());
                     }
                 }
 
-                if (numberOfAssignments != 0) {
-                    builder.setSmallIcon(R.drawable.ic_logo);
-                    builder.setAutoCancel(true);
-
-                    //to be able to launch your activity from the notification
-                    Intent notifyIntent = new Intent(context, MainActivity.class);
-                    PendingIntent pendingIntent = PendingIntent.getActivity(context, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    builder.setContentIntent(pendingIntent);
-                    builder.setSound(Uri.parse(prefs.getString("notifications_new_message_ringtone", "content://settings/system/notification_sound")));
-                    if (prefs.getBoolean("notifications_new_message_vibrate", true)) {
-                        builder.setVibrate(new long[]{1000});
-                    } else {
-                        builder.setVibrate(new long[]{0});
-                    }
-
-                    inboxStyle.setBigContentTitle(numberOfAssignments + " assignments due tomorrow");
-                    inboxStyle.setSummaryText(numberOfAssignments + " assignments due tomorrow");
-                    builder.setStyle(inboxStyle);
-
-                    NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
-                    managerCompat.notify(NOTIFICATION_ID, builder.build());
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
                 }
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-
-        });
+            });
+        }
     }
 }
