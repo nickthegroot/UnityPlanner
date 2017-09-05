6f17ba76431afa063dd98f72d35eb71cf5d45284
Nick DeGroot <nbdegroot1@gmail.com>
2017-09-05 11:20:34 -0700

    Added swipe to set as complete for assignment tab

--- android/app/src/main/java/com/nbdeg/unityplanner/AssignmentList.java
+++ android/app/src/main/java/com/nbdeg/unityplanner/AssignmentList.java
@@ -1,11 +1,18 @@
 package com.nbdeg.unityplanner;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +33,9 @@ import java.util.Date;
 public class AssignmentList extends Fragment {
 
     FirebaseRecyclerAdapter mAdapter;
+    RecyclerView recyView;
+
+    private Paint p = new Paint();
 
     public AssignmentList() {
         // Required empty public constructor
@@ -45,10 +55,10 @@ public class AssignmentList extends Fragment {
                              Bundle savedInstanceState) {
 
         View mView = inflater.inflate(R.layout.fragment_assignment_list, container, false);
-        RecyclerView assignmentView = (RecyclerView) mView.findViewById(R.id.assignment_list_view);
+        recyView = (RecyclerView) mView.findViewById(R.id.assignment_list_view);
         LinearLayoutManager assignmentLayoutManager = new LinearLayoutManager(getContext());
 
-        assignmentView.setLayoutManager(assignmentLayoutManager);
+        recyView.setLayoutManager(assignmentLayoutManager);
 
 
         Query ref = Database.dueAssignmentDb.orderByChild("dueDate");
@@ -74,7 +84,8 @@ public class AssignmentList extends Fragment {
             }
         };
 
-        assignmentView.setAdapter(mAdapter);
+        recyView.setAdapter(mAdapter);
+        initSwipe();
 
         // Inflate the layout for this fragment
         return mView;
@@ -84,5 +95,51 @@ public class AssignmentList extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         mAdapter.cleanup();
+    }
+
+    private void initSwipe(){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+
+                Assignment oldAssignment = (Assignment) mAdapter.getItem(position);
+                Assignment newAssignment = new Assignment(oldAssignment);
+                newAssignment.setPercentComplete(100);
+                Database.editAssignment(newAssignment, oldAssignment, getContext());
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    Assignment assignment = (Assignment) mAdapter.getItem(viewHolder.getAdapterPosition());
+
+                    if(dX > 0){
+                        p.setColor(assignment.getDueCourse().getColor());
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_check_white);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyView);
     }
 }
